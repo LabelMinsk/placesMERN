a9581ae1378d7891031d2ed7c7e5c44a8a0b1757
Mikhail Karnou <33331982+LabelMinsk@users.noreply.github.com>
2020-09-28 14:41:02 +0300

    complete input validation

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import Input from '../../shared/components/FormElements/Input';
+import {VALIDATOR_REQUIRE} from '../../shared/components/util/validators';
 
-import './PlaceForm.css'
+import './PlaceForm.css';
 
 const NewPlace = () => {
   return (
-    <form className='place-form'>
-      <Input element='input' type={'text'} label='Title'/>
+    <form className="place-form">
+      <Input
+        element="input"
+        type={'text'}
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title"
+      />
     </form>
-  )
+  );
 };
 
 export default NewPlace;
